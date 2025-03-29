79da551df7d0fbfefd285cb357affa4cb1662019
Taha-Sayyed <codetaha786@gmail.com>
2025-03-29 16:06:31 +0530

    Intial Commit

---
+++ Train_Network/main_dropout.py
@@ -0,0 +1,72 @@
+
+import os
+import numpy as np
+import tensorflow as tf
+import input_getbatch
+import model_c
+#model_a,model_b and model_c
+
+
+N_CLASSES = 2
+IMG_W = 96  
+IMG_H = 96
+
+BATCH_SIZE = 16
+CAPACITY = 4000
+MAX_STEP = 4000 
+learning_rate = 0.0001 
+
+
+def run_training():
+    
+
+    train_dir = 'D:\\database\\train\\'   
+    logs_train_dir = 'D:\\train_myself\\model_testc\\'  
+    
+    train, train_label = input_getbatch.get_files(train_dir)    
+    train_batch, train_label_batch = input_getbatch.get_batch(train,
+                                                  train_label,
+                                                  IMG_W,
+                                                  IMG_H,
+                                                  BATCH_SIZE, 
+                                                  CAPACITY)
+    
+    
+    x = tf.placeholder(tf.float32, shape=[BATCH_SIZE, IMG_W, IMG_H, 3])  
+    y_ = tf.placeholder(tf.int32, shape=[BATCH_SIZE])     
+    
+    logits = model_c.inference(x, BATCH_SIZE, N_CLASSES,0.5)
+    loss = model_c.losses(logits, y_)  
+    acc = model_c.evaluation(logits, y_)
+    train_op = model_c.trainning(loss, learning_rate)
+    
+    
+             
+    with tf.Session() as sess:
+        saver = tf.train.Saver()    
+        sess.run(tf.global_variables_initializer())    
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess= sess, coord=coord)
+
+        try:
+            for step in np.arange(MAX_STEP):
+                if coord.should_stop():
+                        break
+                
+                tra_images,tra_labels = sess.run([train_batch, train_label_batch])
+                _, tra_loss, tra_acc = sess.run([train_op, loss, acc],
+                                                feed_dict={x:tra_images, y_:tra_labels})
+                                       
+                if step % 100 == 0:
+                    print('Step %d, train loss = %.2f, train accuracy = %.2f%%' %(step, tra_loss, tra_acc*100.0))
+
+                if step % 2000 == 0 or (step + 1) == MAX_STEP:
+                    checkpoint_path = os.path.join(logs_train_dir, 'model.ckpt')
+                    saver.save(sess, checkpoint_path, global_step=step)
+                    
+        except tf.errors.OutOfRangeError:
+            print('Done training -- epoch limit reached')
+        finally:
+            coord.request_stop()           
+        coord.join(threads)
+        
---
+++ Train_Network/model_c.py
@@ -0,0 +1,266 @@
+import tensorflow as tf
+
+def _variable_on_cpu(name, shape, initializer):
+    
+    with tf.device('/cpu:0'):
+        var = tf.get_variable(name, shape, initializer=initializer)
+    return var
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+  
+    var = _variable_on_cpu(name, shape,
+                           tf.contrib.layers.variance_scaling_initializer(
+                                                   factor=1.0,
+                                                   mode='FAN_IN',
+                                                   uniform=True,
+                                                   dtype=tf.float32
+                                                           ))
+
+    weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+    
+    # Stores value in the collection with the given name
+    tf.add_to_collection('losses', weight_decay)
+    return var
+#%%
+def inference(images, batch_size, n_classes,dropout):
+   
+
+    with tf.variable_scope('conv1') as scope:
+        weights = tf.get_variable('weights', 
+                                  shape = [7,7,3,64],
+                                  dtype = tf.float32, 
+                                  initializer=tf.contrib.layers.variance_scaling_initializer(
+                                                   factor=1.0,
+                                                   mode='FAN_IN',
+                                                   uniform=True,
+                                                   dtype=tf.float32
+                                                           ))
+        biases = tf.get_variable('biases', 
+                                 shape=[64],
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(images, weights, strides=[1,1,1,1], padding='SAME')
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv1 = tf.nn.relu(pre_activation, name= scope.name)
+
+    
+    with tf.variable_scope('pooling1_lrn') as scope:
+        pool1 = tf.nn.max_pool(conv1, ksize=[1,2,2,1],strides=[1,2,2,1],
+                               padding='SAME', name='pooling1')
+        pool1_dropout=tf.nn.dropout(pool1,dropout,name="pool1_dropout") 
+    
+
+    with tf.variable_scope('conv2') as scope:
+        weights = tf.get_variable('weights',
+                                  shape=[5,5,64,128],
+                                  dtype=tf.float32,
+                                  initializer=tf.contrib.layers.variance_scaling_initializer(
+                                                   factor=1.0,
+                                                   mode='FAN_IN',
+                                                   uniform=True,
+                                                   dtype=tf.float32
+                                                           ))
+        biases = tf.get_variable('biases',
+                                 shape=[128], 
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(pool1_dropout, weights, strides=[1,1,1,1],padding='SAME')
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(pre_activation, name='conv2')
+    
+
+    with tf.variable_scope('pooling2_lrn') as scope:
+        pool2 = tf.nn.max_pool(conv2, ksize=[1,2,2,1],strides=[1,2,2,1],
+                               padding='SAME', name='pooling2')
+        pool2_dropout=tf.nn.dropout(pool2,dropout,name="pool2_dropout") 
+        
+
+    with tf.variable_scope('conv3_1') as scope:
+        weights = tf.get_variable('weights',
+                                  shape=[3,3,128,256],
+                                  dtype=tf.float32,
+                                  initializer=tf.contrib.layers.variance_scaling_initializer(
+                                                   factor=1.0,
+                                                   mode='FAN_IN',
+                                                   uniform=True,
+                                                   dtype=tf.float32
+                                                           ))
+        biases = tf.get_variable('biases',
+                                 shape=[256], 
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(pool2_dropout, weights, strides=[1,1,1,1],padding='SAME')
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv3_1 = tf.nn.relu(pre_activation, name='conv3_1')
+        
+    with tf.variable_scope('conv3_2') as scope:
+        weights = tf.get_variable('weights',
+                                  shape=[3,3,256,256],
+                                  dtype=tf.float32,
+                                  initializer=tf.contrib.layers.variance_scaling_initializer(
+                                                   factor=1.0,
+                                                   mode='FAN_IN',
+                                                   uniform=True,
+                                                   dtype=tf.float32
+                                                           ))
+        biases = tf.get_variable('biases',
+                                 shape=[256], 
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(conv3_1, weights, strides=[1,1,1,1],padding='SAME')
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv3_2 = tf.nn.relu(pre_activation, name='conv3_2')
+       
+    
+    with tf.variable_scope('pooling3_lrn') as scope:
+        pool3 = tf.nn.max_pool(conv3_2, ksize=[1,2,2,1], strides=[1,1,1,1],
+                               padding='SAME',name='pooling3')
+        pool3_dropout=tf.nn.dropout(pool3,dropout,name="pool3_dropout") 
+            
+
+    with tf.variable_scope('conv4_1') as scope:
+        weights = tf.get_variable('weights',
+                                  shape=[3,3,256,512],
+                                  dtype=tf.float32,
+                                  initializer=tf.contrib.layers.variance_scaling_initializer(
+                                                   factor=1.0,
+                                                   mode='FAN_IN',
+                                                   uniform=True,
+                                                   dtype=tf.float32
+                                                           ))
+        biases = tf.get_variable('biases',
+                                 shape=[512], 
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(pool3_dropout, weights, strides=[1,1,1,1],padding='SAME')
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv4_1 = tf.nn.relu(pre_activation, name='conv4_1')
+        
+    with tf.variable_scope('conv4_2') as scope:
+        weights = tf.get_variable('weights',
+                                  shape=[3,3,512,512],
+                                  dtype=tf.float32,
+                                  initializer=tf.contrib.layers.variance_scaling_initializer(
+                                                   factor=1.0,
+                                                   mode='FAN_IN',
+                                                   uniform=True,
+                                                   dtype=tf.float32
+                                                           ))
+        biases = tf.get_variable('biases',
+                                 shape=[512], 
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(conv4_1, weights, strides=[1,1,1,1],padding='SAME')
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv4_2 = tf.nn.relu(pre_activation, name='conv4_2')    
+    
+    
+    with tf.variable_scope('pooling4_lrn') as scope:
+        pool4 = tf.nn.max_pool(conv4_2, ksize=[1,2,2,1], strides=[1,1,1,1],
+                               padding='SAME',name='pooling4')
+        pool4_dropout=tf.nn.dropout(pool4,dropout,name="pool4_dropout") 
+         
+            
+    with tf.variable_scope('conv5_1') as scope:
+        weights = tf.get_variable('weights',
+                                  shape=[3,3,512,512],
+                                  dtype=tf.float32,
+                                  initializer=tf.contrib.layers.variance_scaling_initializer(
+                                                   factor=1.0,
+                                                   mode='FAN_IN',
+                                                   uniform=True,
+                                                   dtype=tf.float32
+                                                           ))
+        biases = tf.get_variable('biases',
+                                 shape=[512], 
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(pool4_dropout, weights, strides=[1,1,1,1],padding='SAME')
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv5_1 = tf.nn.relu(pre_activation, name='conv5_1')
+        
+    with tf.variable_scope('conv5_2') as scope:
+        weights = tf.get_variable('weights',
+                                  shape=[3,3,512,512],
+                                  dtype=tf.float32,
+                                  initializer=tf.contrib.layers.variance_scaling_initializer(
+                                                   factor=1.0,
+                                                   mode='FAN_IN',
+                                                   uniform=True,
+                                                   dtype=tf.float32
+                                                           ))
+        biases = tf.get_variable('biases',
+                                 shape=[512], 
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(conv5_1, weights, strides=[1,1,1,1],padding='SAME')
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv5_2 = tf.nn.relu(pre_activation, name='conv5_2')
+       
+
+    with tf.variable_scope('pooling5_lrn') as scope:
+        pool5 = tf.nn.max_pool(conv5_2, ksize=[1,2,2,1], strides=[1,1,1,1],
+                               padding='SAME',name='pooling5')
+        pool5_dropout=tf.nn.dropout(pool5,dropout,name="pool5_dropout") 
+       
+    with tf.variable_scope('local3') as scope:
+        reshape = tf.reshape(pool5_dropout, shape=[batch_size, -1])
+        dim = reshape.get_shape()[1].value
+        weights = _variable_with_weight_decay('weights', shape=[dim,128],
+                                             stddev=0.1, wd=0.005)
+        biases = tf.get_variable('biases',
+                                 shape=[128],
+                                 dtype=tf.float32, 
+                                 initializer=tf.constant_initializer(0.1))
+        local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+        local3_dropout=tf.nn.dropout(local3,dropout,name="local3_dropout") 
+    
+    with tf.variable_scope('local4') as scope:
+        weights = _variable_with_weight_decay('weights', shape=[128,128],
+                                             stddev=0.1, wd=0.005)
+        biases = tf.get_variable('biases',
+                                 shape=[128],
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        local4 = tf.nn.relu(tf.matmul(local3_dropout, weights) + biases, name='local4')
+        local4_dropout=tf.nn.dropout(local4,dropout,name="local4_dropout") 
+   
+    with tf.variable_scope('softmax_linear') as scope:
+        weights = _variable_with_weight_decay('softmax_linear', shape=[128,n_classes],
+                                             stddev=0.1, wd=0.005)
+        biases = tf.get_variable('biases', 
+                                 shape=[n_classes],
+                                 dtype=tf.float32, 
+                                 initializer=tf.constant_initializer(0.1))
+        softmax_linear = tf.add(tf.matmul(local4_dropout, weights), biases, name='softmax_linear')
+    
+    return softmax_linear
+
+#%%
+def losses(logits, labels):
+
+    with tf.variable_scope('loss') as scope:
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits\
+                        (logits=logits, labels=labels, name='xentropy_per_example')
+        loss = tf.reduce_mean(cross_entropy, name='loss')
+        tf.summary.scalar(scope.name+'/loss', loss)
+    return loss
+
+#%%
+def trainning(loss, learning_rate):
+
+    with tf.name_scope('optimizer'):
+        optimizer = tf.train.AdamOptimizer(learning_rate= learning_rate)
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+        train_op = optimizer.minimize(loss, global_step= global_step)
+    return train_op
+
+#%%
+def evaluation(logits, labels):
+
+  with tf.variable_scope('accuracy') as scope:
+      correct = tf.nn.in_top_k(logits, labels, 1)
+      correct = tf.cast(correct, tf.float16)
+      accuracy = tf.reduce_mean(correct)
+      tf.summary.scalar(scope.name+'/accuracy', accuracy)
+  return accuracy
